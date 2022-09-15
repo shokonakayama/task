a9c6fcf29fcc93aa19868a1f65c415082631e1cb
shokonakayama <shokochi0208@gmail.com>
2022-09-15 09:58:08 +0900

    登録処理完成

--- config.php
+++ config.php
@@ -5,6 +5,9 @@ define('DSN', 'mysql:host=db;dbname=task_app;charset=utf8');
 define('USER', 'task_admin');
 define('PASSWORD', '1234');
 
+// エラーメッセージを定数として定義
+define('MSG_TITLE_REQUIRED', 'タスク名を入力してください');
+
 // doneの状態を定数として定義
 define('TASK_NOTYET', 0);
 define('TASK_DONE', 1);
